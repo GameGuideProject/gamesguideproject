6a96d8b8598f9a7328e87e1e3908c28553d5342e
zaidHabiba <35657365+zaidHabiba@users.noreply.github.com>
2018-03-30 15:56:26 +0200

    Update LoadGameRectangle.js

--- Project/Script/LoadGameRectangle.js
+++ Project/Script/LoadGameRectangle.js
@@ -1,7 +1,7 @@
 function LoadGames(Objectbody) {
 
     var imagecount=6;
-    alert(""+(((imagecount*300)/3)+50)+"px");
+   /* alert(""+(((imagecount*300)/3)+50)+"px");*/
     Objectbody.style.height=""+(((imagecount*300)/3)+150)+"px";
     Objectbody.innerHTML=
         '<div class="RectangleGameViewBodyStyle"> <div> <img class="RectangleGameViewIconStyle" src="Image/Icon/game2.png"> </div> <img class="RectangleGameViewImageStyle" src="Image/images/killzone.jpg" width="100%"> <div class="RectangleGameViewNameStyle"><b>Kill Zone</b> </div><div ><div class="RectangleGameViewDescription"> This game is build omg jkk hhh  loll </div></div></div>'
